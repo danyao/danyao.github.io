50cb4f06a878cbbc2beae84befffbdfda564d8da
Danyao Wang <danyao@google.com>
2017-05-16 17:14:14 -0400

    Add nav_tester example

---
+++ nav_tester/README.md
@@ -0,0 +1,14 @@
+# Navigation Interface Tests
+
+Here you can find a small tester page for navigation behaviors in browsers.
+
+`iframes.html` is the main tester page. It contains two levels of iframes in
+which you can test navigation behaviors.
+
+Nesting structure:
+
+* iframes.html
+  * nav_tester_with_frame.html
+    * nav_tester.html
+
+Each page includes `nav_tester.js`, which generates the test UI.
---
+++ nav_tester/nav_tester.js
@@ -0,0 +1,50 @@
+function loadTestPage() {
+  var containerDiv = document.querySelector("div#tester");
+  var testMethods = [
+    "location.href",
+    "location.assign()",
+    "location.replace()",
+    "location.reload()",
+    "history.pushState()",
+    "history.replaceState()",
+    "history.back()",
+    "history.forward()",
+    "dump()"];
+
+  var html = [];
+  for (var i in testMethods) {
+    var method = testMethods[i];
+    html.push('<p><input type="button" onclick="run(\'' + method + '\')" value="' + method + '"></p>');
+  }
+  containerDiv.innerHTML = html.join('\n');
+}
+
+function run(cmd) {
+  if (cmd == "location.href") {
+    location.href = "1.html?q=location-href";
+  } else if (cmd == "location.assign()") {
+    location.assign("1.html?q=location-assign");
+  } else if (cmd == "location.replace()") {
+    location.replace("1.html?q=location-replace");
+  } else if (cmd == "location.reload()") {
+    location.reload();
+  } else if (cmd == "history.pushState()") {
+    history.pushState({name: "pushState"}, "Unused Title", "1.html?q=history-pushState");
+  } else if (cmd == "history.replaceState()") {
+    history.replaceState({name: "replaceState"}, "Unused Title", "1.html?q=history-replaceState");
+  } else if (cmd == "history.back()") {
+    history.back();
+  } else if (cmd == "history.forward()") {
+    history.forward();
+  } else if (cmd == "dump()") {
+    dump();
+  } else {
+    console.log("Unknown command: ", cmd);
+  }
+}
+
+function dump() {
+  console.log("location.href: ", location.href);
+  console.log("history.length: ", history.length);
+  console.log("history.state: ", history.state);
+}
