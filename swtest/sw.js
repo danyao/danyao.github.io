64875888b7e64f5ec9d8ca2007cc11d7a0419cdb
Danyao Wang <danyao@chromium.org>
2018-01-26 17:09:36 -0500

    fix missing rename

--- swtest/sw.js
+++ swtest/sw.js
@@ -34,7 +34,7 @@ self.addEventListener('fetch', function(event) {
         });
         return response;
       }).catch(function () {
-        return caches.match('/sw-test/gallery/myLittleVader.jpg');
+        return caches.match('/swtest/gallery/myLittleVader.jpg');
       });
     }
   }));
